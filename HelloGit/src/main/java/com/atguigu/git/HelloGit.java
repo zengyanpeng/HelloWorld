824cccd23481b8156ce9f83ed90dcd88031d3750
zengyanpeng <530878210@qq.com>
2022-06-28 16:21:42 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author pengzy
+ * @create 2022-06-28 16:10
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
